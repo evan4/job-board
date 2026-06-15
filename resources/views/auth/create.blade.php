23192dd8aab9e5ebe1983f6040258707f538a642
Ivan Dedov <evan4mc@gmail.com>
2026-06-15 17:39:05 +0500

    create auth controller, add routes & view for login page

---
+++ resources/views/auth/create.blade.php
@@ -0,0 +1,5 @@
+<x-layout>
+    <x-card>
+        <h1>Login</h1>
+    </x-card>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\JobsListController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('', fn() => to_route('jobs.index'));
+
+Route::get('login', fn() => to_route('auth.create'))->name('login');
+Route::resource('auth', AuthController::class)->only(['create', 'store']);
+
 Route::resource('jobs', JobsListController::class)->only(['index', 'show']);
+
